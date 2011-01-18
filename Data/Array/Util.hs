fa12f8de906656fdff592aecd4d677fa27f1c908
Alex Mason <axman6@gmail.com>
2011-01-19 00:25:54 +0900

    emoved forM_, fixed logic errors, removed redundant Ix import

--- Data/Array/Util.hs
+++ Data/Array/Util.hs
@@ -8,7 +8,6 @@ module Data.Array.Util
 
 import Data.Array.Base
 import Data.Array.MArray
-import Data.Ix
 import Control.Monad
 
 {-# INLINE mapElems #-}
@@ -18,18 +17,29 @@ mapElems :: (MArray a e m, Ix i) => (e -> e) -> a i e -> m ()
 mapElems f arr = do
     (start, end') <- getBounds arr
     let !end = index (start, end') end'
-    forM_ [0..end] $ \i ->
-        unsafeRead arr i >>= unsafeWrite arr i . f
+        go !i | i <= end = do
+            e <- unsafeRead arr i
+            unsafeWrite arr i $! (f e)
+            go (i+1)
+        go _  = return ()
+    go 0
+
+
 
 {-# INLINE mapElemsIx #-}
 -- | The same as mapElems, but also providing the index to the
 -- mapping function. /O(size of arr)/
 mapElemsIx :: (MArray a e m, Ix i) => (i -> e -> e) -> a i e -> m ()
 mapElemsIx f arr =do
-    (start, end') <- getBounds arr
+    bnds@(start, end') <- getBounds arr
     let !end = index (start, end') end'
-    forM_ (zip [0..end] (range (start, end'))) $ \(i,i') ->
-        unsafeRead arr i >>= unsafeWrite arr i . f i'
+        -- Should be guaranteed that xs will be null when i = end+1
+        go !i (!x:xs) | i <= end = do
+            e <- unsafeRead arr i
+            unsafeWrite arr i $! f x e
+            go (i+1) xs
+        go _ _ = return ()
+    go 0 (range bnds)
 
 {-# INLINE mapElems' #-}
 -- | Takes a mapping function, and a list of indicies to mutate.
@@ -37,23 +47,32 @@ mapElemsIx f arr =do
 -- are out of range. /O(length xs)/
 mapElems' :: (MArray a e m, Ix i) => (e -> e) -> [i] -> a i e -> m Bool
 mapElems' f xs arr = do
-    (lo, hi) <- getBounds arr
-    let toOffset = index (lo, hi)
-        ok = not (all (\x -> x >= lo && x <= hi) xs)
-    when ok $ forM_ (map toOffset xs) $ \i ->
-        unsafeRead arr i >>= unsafeWrite arr i . f
+    bnds@(lo, hi) <- getBounds arr
+    let !ok = all (inRange bnds) xs
+        toOffset = index (lo, hi)
+        go (!i:is) = do
+            e <- unsafeRead arr i
+            unsafeWrite arr i $! f e
+            go is
+        go _ = return ()
+    when ok $ go (map toOffset xs)
     return ok
 
 
 {-# INLINE mapElemsIx' #-}
--- | Takes a mapping function, and a list of indicies to mutate.
--- Returns True if the muration occured, and False if the indicies
+-- | Takes a mapping function which takes an index, and a list of indicies
+-- to mutate. Returns True if the muration occured, and False if the indicies
 -- are out of range. /O(length xs)/
 mapElemsIx' :: (MArray a e m, Ix i) => (i -> e -> e) -> [i] -> a i e -> m Bool
 mapElemsIx' f xs arr = do
-    (lo, hi) <- getBounds arr
-    let toOffset = index (lo, hi)
-        ok = not (all (\x -> x >= lo && x <= hi) xs)
-    when ok $ forM_ (zip (map toOffset xs) xs) $ \(i,i') ->
-        unsafeRead arr i >>= unsafeWrite arr i . f i'
+    bnds@(lo, hi) <- getBounds arr
+    let !ok =  all (inRange bnds) xs
+        toOffset = index (lo, hi)
+        ixs = map toOffset xs
+        go (!i:is) (!i':is') = do
+            e <- unsafeRead arr i
+            unsafeWrite arr i $! f i' e
+            go is is'
+        go _ _ = return ()
+    when ok $ go ixs xs
     return ok
