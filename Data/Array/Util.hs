e631bf6f9c0e5cdb99b51e92d67892df6a3a2791
Alex Mason <axman6@gmail.com>
2011-01-19 01:03:37 +0900

    Changes thanks to #ghc comments, back yo forM_

--- Data/Array/Util.hs
+++ Data/Array/Util.hs
@@ -1,70 +1,59 @@
 {-# LANGUAGE BangPatterns #-}
 module Data.Array.Util
-    ( mapElems
-    , mapElemsIx
-    , mapElems'
-    , mapElemsIx'
+    ( updateElems
+    , updateElemsIx
+    , updateElems'
+    , updateElemsIx'
     ) where
 
 import Data.Array.Base
 import Data.Array.MArray
 import Control.Monad
+import Control.Exception
 
-{-# INLINE mapElems #-}
--- | mapElems mutates every element in an array while avoiding
+{-# INLINE updateElems #-}
+-- | updateElems mutates every element in an array while avoiding
 -- all bounds checks. /O(size of arr)/
-mapElems :: (MArray a e m, Ix i) => (e -> e) -> a i e -> m ()
-mapElems f arr = do
+updateElems :: (MArray a e m, Ix i) => (e -> e) -> a i e -> m ()
+updateElems f arr = do
     (start, end') <- getBounds arr
     let !end = index (start, end') end'
-        go !i | i <= end = do
-            e <- unsafeRead arr i
-            unsafeWrite arr i $! (f e)
-            go (i+1)
-        go _  = return ()
-    go 0
+    forM_ [0..end] $ \i -> unsafeRead arr i >>= unsafeWrite arr i . f
 
 
 
-{-# INLINE mapElemsIx #-}
--- | The same as mapElems, but also providing the index to the
+{-# INLINE updateElemsIx #-}
+-- | The same as updateElems, but also providing the index to the
 -- mapping function. /O(size of arr)/
-mapElemsIx :: (MArray a e m, Ix i) => (i -> e -> e) -> a i e -> m ()
-mapElemsIx f arr =do
+updateElemsIx :: (MArray a e m, Ix i) => (i -> e -> e) -> a i e -> m ()
+updateElemsIx f arr =do
     bnds@(start, end') <- getBounds arr
     let !end = index (start, end') end'
         -- Should be guaranteed that xs will be null when i = end+1
         go !i (!x:xs) | i <= end = do
             e <- unsafeRead arr i
-            unsafeWrite arr i $! f x e
+            unsafeWrite arr i $ f x e
             go (i+1) xs
         go _ _ = return ()
     go 0 (range bnds)
 
-{-# INLINE mapElems' #-}
+{-# INLINE updateElems' #-}
 -- | Takes a mapping function, and a list of indicies to mutate.
--- Returns True if the muration occured, and False if the indicies
--- are out of range. /O(length xs)/
-mapElems' :: (MArray a e m, Ix i) => (e -> e) -> [i] -> a i e -> m Bool
-mapElems' f xs arr = do
-    bnds@(lo, hi) <- getBounds arr
+-- /O(length xs)/
+updateElems' :: (MArray a e m, Ix i) => (e -> e) -> [i] -> a i e -> m ()
+updateElems' f xs arr = do
+    bnds <- getBounds arr
     let !ok = all (inRange bnds) xs
-        toOffset = index (lo, hi)
-        go (!i:is) = do
-            e <- unsafeRead arr i
-            unsafeWrite arr i $! f e
-            go is
-        go _ = return ()
-    when ok $ go (map toOffset xs)
-    return ok
+        toOffset = index bnds
+    when (not ok) $ throw (IndexOutOfBounds $ "Data.Array.Util updateElems': Index out of bounds")
+    forM_ (map toOffset xs) $ \i -> unsafeRead arr i >>= unsafeWrite arr i . f
 
 
-{-# INLINE mapElemsIx' #-}
+{-# INLINE updateElemsIx' #-}
 -- | Takes a mapping function which takes an index, and a list of indicies
--- to mutate. Returns True if the muration occured, and False if the indicies
--- are out of range. /O(length xs)/
-mapElemsIx' :: (MArray a e m, Ix i) => (i -> e -> e) -> [i] -> a i e -> m Bool
-mapElemsIx' f xs arr = do
+-- to mutate. /O(length xs)/
+updateElemsIx' :: (MArray a e m, Ix i) => (i -> e -> e) -> [i] -> a i e -> m ()
+updateElemsIx' f xs arr = do
     bnds@(lo, hi) <- getBounds arr
     let !ok =  all (inRange bnds) xs
         toOffset = index (lo, hi)
@@ -74,5 +63,5 @@ mapElemsIx' f xs arr = do
             unsafeWrite arr i $! f i' e
             go is is'
         go _ _ = return ()
-    when ok $ go ixs xs
-    return ok
+    when (not ok) $ throw (IndexOutOfBounds $ "Data.Array.Util updateElemsIx': Index out of bounds")
+    go ixs xs
